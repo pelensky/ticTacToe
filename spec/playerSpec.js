36a3c8a2348af4d1e5be30b75944b7792bc09fd1
Dan Pelensky <dan@pelensky.com>
2017-01-26 10:29:02 +0000

    players have a turn which can be true or false

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -7,8 +7,12 @@ describe("Player", function() {
     player2 = new Player("Courtney");
   });
 
-  it("should initialize with two players", function() {
+  it("Should take a name as an argument", function() {
     expect(player1.name).toEqual("Dan");
     expect(player2.name).toEqual("Courtney");
+  });
+
+  it("Should know if it is the player's turn", function(){
+    expect(player1.isTurn).toBe(false);
   });
 });
