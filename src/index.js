21317a3123b865ac85db2997dd9208a12b3b55c8
Dan Pelensky <dan@pelensky.com>
2017-01-26 16:50:34 +0000

    refactored out a choosing a space method

--- src/index.js
+++ src/index.js
@@ -9,109 +9,80 @@ $(document).ready(function() {
     var player1 = new Player(player1Name);
     var player2 = new Player(player2Name);
     game.addPlayers(player1, player2);
-    console.log(game);
     game.startGame();
     $('#names').html(`${player1.name} vs ${player2.name}`);
   });
 
   $("#a1Link").on("click", function(){
-    game.chooseSpace("a1");
-    if(game.currentPlayer === game.player1.name){
-      $('#a1').html(`X`);
-    } else {
-      $('#a1').html(`O`);
-    }
-    check_game_over();
+    var space = "a1";
+    choosingASpace(space);
+    checkGameOver();
   });
 
   $("#a2Link").on("click", function(){
-    game.chooseSpace("a2");
+    var space = "a2";
+    choosingASpace(space);
+  });
+
+  $("#a3Link").on("click", function(){
+    var space = "a3";
+    choosingASpace(space);
+    checkGameOver();
+
+  });
+
+  $("#b1Link").on("click", function(){
+    var space = "b1";
+    choosingASpace(space);
+    checkGameOver();
+
+  });
+
+  $("#b2Link").on("click", function(){
+    var space = "b2";
+    choosingASpace(space);
+    checkGameOver();
+
+  });
+
+  $("#b3Link").on("click", function(){
+    var space = "b3";
+    choosingASpace(space);
+    checkGameOver();
+
+  });
+
+  $("#c1Link").on("click", function(){
+    var space = "c1";
+    choosingASpace(space);
+    checkGameOver();
+  });
+
+  $("#c2Link").on("click", function(){
+    var space = "c2";
+    choosingASpace(space);
+    checkGameOver();
+  });
+
+  $("#c3Link").on("click", function(){
+    var space = "c3";
+    choosingASpace(space);
+    checkGameOver();
+  });
+
+  function choosingASpace(space){
+    game.chooseSpace(space);
     if(game.currentPlayer === game.player1.name){
-      $('#a2').html(`X`);
+      $("#" + space).html(`X`);
     } else {
-      $('#a2').html(`O`);
+      $("#" + space).html(`O`);
     }
-    check_game_over();
-  });
+  }
 
-    $("#a3Link").on("click", function(){
-      game.chooseSpace("a3");
-      if(game.currentPlayer === game.player1.name){
-        $('#a3').html(`X`);
-      } else {
-        $('#a3').html(`O`);
-      }
-      check_game_over();
-
-    });
-
-    $("#b1Link").on("click", function(){
-      game.chooseSpace("b1");
-      if(game.currentPlayer === game.player1.name){
-        $('#b1').html(`X`);
-      } else {
-        $('#b1').html(`O`);
-      }
-      check_game_over();
-
-    });
-
-    $("#b2Link").on("click", function(){
-      game.chooseSpace("b2");
-      if(game.currentPlayer === game.player1.name){
-        $('#b2').html(`X`);
-      } else {
-        $('#b2').html(`O`);
-      }
-      check_game_over();
-
-    });
-
-    $("#b3Link").on("click", function(){
-      game.chooseSpace("b3");
-      if(game.currentPlayer === game.player1.name){
-        $('#b3').html(`X`);
-      } else {
-        $('#b3').html(`O`);
-      }
-      check_game_over();
-
-    });
-
-    $("#c1Link").on("click", function(){
-      game.chooseSpace("c1");
-      if(game.currentPlayer === game.player1.name){
-        $('#c1').html(`X`);
-      } else {
-        $('#c1').html(`O`);
-      }
-      check_game_over();
-    });
-
-    $("#c2Link").on("click", function(){
-      game.chooseSpace("c2");
-      if(game.currentPlayer === game.player1.name){
-        $('#c2').html(`X`);
-      } else {
-        $('#c2').html(`O`);
-      }
-      check_game_over();
-    });
-
-    $("#c3Link").on("click", function(){
-      game.chooseSpace("c3");
-      if(game.currentPlayer === game.player1.name){
-        $('#c3').html(`X`);
-      } else {
-        $('#c3').html(`O`);
-      }
-      check_game_over();
-    });
-
-    function check_game_over() {
-      if(game.gameOver){
-        $('h1').html(`${game.winner} is the almighty winner!`);
-        $( "a" ).remove();
-      }
+  function checkGameOver() {
+    if(game.gameOver){
+      $('h1').html(`${game.winner} is the almighty winner!`);
+      $( "a" ).remove();
     }
-  });
+  }
+});
